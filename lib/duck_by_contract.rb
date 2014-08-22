70ef38fed3e581b69c7c3c1a67f61c92fc9357e2
Michael J. Gilliland <mjg.py3@gmail.com>
2014-08-22 08:42:46 -0400

    Revert "slight refactoring"
    This reverts commit e34b48f6e0df11ef2d7981e9d7e0ba0c80bbc849.

--- lib/duck_by_contract.rb
+++ lib/duck_by_contract.rb
@@ -3,10 +3,9 @@ module DuckByContract
   def duck_type(type_hash)
     method_name = type_hash.keys.first
     old_method = instance_method(method_name)
-    duck_methods = type_hash.values.first
 
     define_method method_name do |*args|
-      raise NotADuck unless args.first.methods.include?(duck_methods.first)
+      raise NotADuck unless args.first.respond_to?(:abs)
       old_method.bind(self).call(*args)
     end
   end
