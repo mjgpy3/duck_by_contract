dadb34b18ee9e5c1fff2c15e6e90dd69569023f7
Michael J. Gilliland <mjg.py3@gmail.com>
2014-08-22 08:00:04 -0400

    Added ok? method just for testing that things work...
    I would rather use this than "to_not raise_error"

--- spec/lib/duck_by_contract_spec.rb
+++ spec/lib/duck_by_contract_spec.rb
@@ -3,7 +3,11 @@ require './lib/duck_by_contract.rb'
 describe DuckByContract do
   subject { extending_class }
 
-  before(:each) { allow(Object).to receive(:ok?).and_return(true) }
+  before(:all) do
+    class Object
+      def ok?; true; end
+    end
+  end
 
   describe 'a class that extends it' do
     let(:extending_class) { Class.new { extend DuckByContract } }
@@ -25,6 +29,16 @@ describe DuckByContract do
       end
 
       it { is_expected.to be_ok }
+
+      describe 'an instance of that class' do
+        let(:an_instance) { extending_class.new }
+
+        context 'when the duck-typed method is called with an object matching the specified duck type' do
+          subject { an_instance.absolute_value(42) }
+
+          it { is_expected.to be_ok }
+        end
+      end
     end
 
   end
