deb7de8f5b3efe1bbe5c2b0a55640ee242abf81d
Michael J. Gilliland <mjg.py3@gmail.com>
2014-08-23 15:16:24 -0400

    Made test pass

--- lib/duck_by_contract.rb
+++ lib/duck_by_contract.rb
@@ -3,9 +3,15 @@ module DuckByContract
   def duck_type(type_hash)
     method_name = type_hash.keys.first
     old_method = instance_method(method_name)
+    methods = type_hash.values.first
+    methods = [methods] unless methods.first.is_a?(Array)
 
     define_method method_name do |*args|
-      raise NotADuck if type_hash.values.first.any? { |m| !args.first.methods.include?(m) }
+      p args
+      p methods
+      args.zip(methods).each do |value, expected_methods|
+        raise NotADuck if Array(expected_methods).any? { |m| !value.methods.include?(m) }
+      end
       old_method.bind(self).call(*args)
     end
   end
